57ba1b473e7c4d89db216734062bd285f3a04dcf
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2010-11-15 23:50:40 +0200

    junity.py: add another test for "-o" option

--- test.sh
+++ test.sh
@@ -2,13 +2,10 @@
 
 RECEIVED="temp.xml"
 
-check_file()
+assert_equal()
 {
-    ARGS=$1
-    EXPECTED=$2
-
-    bin/junity -o $RECEIVED $ARGS
-
+    EXPECTED=$1
+    
     diff $RECEIVED $EXPECTED > /dev/null 2>&1
     if [ $? == 0 ]
     then
@@ -20,34 +17,62 @@ check_file()
     rm -f $RECEIVED
 }
 
-check_stdout()
+assert_file_equal1()
 {
     ARGS=$1
     EXPECTED=$2
 
+    write_to_file $ARGS
+    assert_equal $EXPECTED
+}
+
+assert_file_equal2()
+{
+    ARGS1=$1
+    ARGS2=$2
+    EXPECTED=$3
+
+    write_to_file $ARGS1
+    write_to_file $ARGS2
+    assert_equal $EXPECTED
+}
+
+assert_stdout_equal()
+{
+    ARGS=$1
+    EXPECTED=$2
+
+    direct_to_file $ARGS
+    assert_equal $EXPECTED
+}
+
+direct_to_file()
+{
+    ARGS=$*
+    
     bin/junity $ARGS > $RECEIVED
-
-    diff $RECEIVED $EXPECTED > /dev/null 2>&1
-    if [ $? == 0 ]
-    then
-        echo "pass"
-    else
-        echo "fail"
-    fi
-
-    rm -f $RECEIVED
 }
 
-check_stdout "foo" "examples/cannot-read-file.xml"
-check_stdout "Makefile" "examples/unknown-file-format.xml"
-check_stdout "junity/junit.py" "examples/bad-file-format.xml"
-check_stdout "test/boost/ExampleTest.xml" "examples/ExampleTest.xml"
-check_stdout "test/boost/boost-report-level.xml" \
+write_to_file()
+{
+    ARGS=$*
+    
+    bin/junity -o $RECEIVED $ARGS
+}
+
+assert_stdout_equal "foo" "examples/cannot-read-file.xml"
+assert_stdout_equal "Makefile" "examples/unknown-file-format.xml"
+assert_stdout_equal "junity/junit.py" "examples/bad-file-format.xml"
+assert_stdout_equal "test/boost/ExampleTest.xml" "examples/ExampleTest.xml"
+assert_stdout_equal "test/boost/boost-report-level.xml" \
     "examples/boost-report-level.xml"
-check_stdout "test/boost/boost-test-log.xml" "examples/boost-test-log.xml"
-check_stdout "test/junit/ExampleTest.xml" "examples/ExampleTest.xml"
-check_stdout "test/titan/TitanTest.log" "examples/TitanTest.xml"
-check_stdout "test/boost/ExampleTest.xml test/boost/ExampleTest.xml" \
+assert_stdout_equal "test/boost/boost-test-log.xml" \
+    "examples/boost-test-log.xml"
+assert_stdout_equal "test/junit/ExampleTest.xml" "examples/ExampleTest.xml"
+assert_stdout_equal "test/titan/TitanTest.log" "examples/TitanTest.xml"
+assert_stdout_equal "test/boost/ExampleTest.xml test/boost/ExampleTest.xml" \
     "examples/ExampleTest-ExampleTest.xml"
-check_file "test/boost/ExampleTest.xml" "examples/ExampleTest.xml"
+assert_file_equal1 "test/boost/ExampleTest.xml" "examples/ExampleTest.xml"
+assert_file_equal2 "test/boost/ExampleTest.xml" "test/boost/ExampleTest.xml" \
+    "examples/ExampleTest-ExampleTest.xml"
 
