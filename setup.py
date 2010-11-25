fb60c058f1d2179cefa8389f97d3c95532e1771c
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2010-11-25 10:31:52 +0200

    junity: release 0.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name="junity",
-      version="0.0.1",
+      version="0.0.2",
       description="JUnity",
       url="https://github.com/jvirtanen/junity",
       author="Jussi Virtanen",
